d3e79a817c1caef4cbd96ae4172cb63f72f42871
Владислав Бочек <76567940+vlad-ister@users.noreply.github.com>
2020-12-30 14:23:03 +0200

    Add files via upload

---
+++ 2.1.5.1.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+using namespace std;
+  int main(void) {
+  int year;
+	cout << "Enter a year: ";
+	cin >> year;
+	  if (year % 4 != 0) {
+		cout << year << " It's a common year.";
+	}
+	else if (year % 100 != 0) {
+		cout << year << " It's a leap year.";
+	}
+	else if (year % 400 != 0) {
+		cout << year << " It's a common year.";
+	}
+	else {
+		cout << year << " It's a leap year.";
+	}
+	return 0;
+}
